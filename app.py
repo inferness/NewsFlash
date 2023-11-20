e6dd6b8659dd493c3d94518aa76d29b66735528a
Ca301 <axelcornelius301@gmail.com>
2023-11-20 09:41:44 +0700

    add category pages and register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
@@ -31,7 +31,17 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[input_required(), Length(min=8, max=80)])
     submit = SubmitField('Submit')
 
-# Register the Blueprint with your app
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[input_required(), Length(min=4, max=15)])
+    email = StringField('Email', validators=[validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', validators=[
+        validators.DataRequired(),
+        validators.Length(min=8),
+        validators.EqualTo('confirm_password', message='Passwords must match')
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[validators.DataRequired()])
+    submit = SubmitField('Submit')
+
 LoggedInUser = User()
 
 # Routes
@@ -41,10 +51,6 @@ def home():
     logged_in_user = session.get('LoggedInUser', None)
     print(logged_in_user)
     return render_template("home.html", user=logged_in_user)
-
-@app.route("/library")
-def library():
-    return render_template("library.html")
 
 @app.route("/team")
 def team():
@@ -70,6 +76,35 @@ def login():
             return redirect(url_for('login'), form=form)
 
     return render_template("login.html", form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        # Create a new user instance with form data
+        existing_user = User.query.filter(
+            (User.email == form.email.data) | (User.username == form.username.data)
+        ).first()
+
+        if (existing_user):
+            return render_template('register.html', form=form)
+            
+        else: 
+            new_user = User(
+                username=form.username.data,
+                email=form.email.data,
+                password=form.password.data
+            )
+
+            # Add the new user to the database
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Redirect to a success page or login page
+            return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 @app.route("/logout", methods=["GET"])
 def logout():
